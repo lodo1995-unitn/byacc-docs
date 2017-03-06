00547ef89eff5bc24a755db0b833ac75d53f4a87
Lodovico Giaretta <lodo1995@users.noreply.github.com>
2017-03-06 11:36:32 +0100

    Fixed make_docs.sh

--- make_docs.sh
+++ make_docs.sh
@@ -3,12 +3,15 @@ then
     doxygen
     git config --global user.email "lodo1995-unitn@users.noreply.github.com"
     git config --global user.name "travis-ci"
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lodo1995-unitn/byacc-docs.git gh-pages
+    cp gh-pages/README.md README.bak
     cd gh-pages
     git rm -rf .
+    cp ../README.bak README.md
     cp -r ../dist/html/* .
     git add -A
     git commit -m "Documentation updated by Travis CI (build $TRAVIS_BUILD_NUMBER)"
-    git push https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages
+    git push https://${GH_TOKEN}@github.com/lodo1995-unitn/byacc-docs.git gh-pages
     cd ..
+    rm README.bak
 fi
